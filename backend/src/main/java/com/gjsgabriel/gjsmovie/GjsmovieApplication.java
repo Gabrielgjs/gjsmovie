f7d2a2768d8e84d3072c0e78fb417d22fa978d4a
Gabrielgjs <gjsgabriel83@gmail.com>
2022-01-10 15:45:18 -0300

    Project created

---
+++ backend/src/main/java/com/gjsgabriel/gjsmovie/GjsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.gjsgabriel.gjsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gjsgabriel/gjsmovie/GjsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjsgabriel.gjsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
